3dce20f2a575995302a3b77abafee8a68446c4c4
Carlos E. Jimenez <cjsaltlake@gmail.com>
2022-07-25 14:01:26 -0400

    Update theme format and icons

--- themes/lab-website-theme/static/js/themetoggle.js
+++ themes/lab-website-theme/static/js/themetoggle.js
@@ -2,12 +2,10 @@ function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
         document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
-        feather.replace()
+        document.getElementById("dark-mode-toggle").innerHTML = "<i>🌞</i>";
     } else if (mode === "light") {
         document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
-        feather.replace()
+        document.getElementById("dark-mode-toggle").innerHTML = "<i>🌜</i>";
     }
 }
 
